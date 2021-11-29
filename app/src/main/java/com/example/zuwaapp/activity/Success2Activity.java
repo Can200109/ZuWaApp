c5b8ebf071c43886f0580c75be35da1938f0fde3
寒笼 <2992261182@qq.com>
2021-11-29 19:26:38 +0800

    还物功能

---
+++ app/src/main/java/com/example/zuwaapp/activity/Success2Activity.java
@@ -0,0 +1,30 @@
+package com.example.zuwaapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.zuwaapp.R;
+
+public class Success2Activity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success2);
+        button = findViewById(R.id.success2);
+
+        getIntent();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
+    }
+}
